0d33252b1e6ab15306cf2e3d2fae97ca211501f8
kamisho <kamisho.message@icloud.com>
2018-09-11 01:19:41 +0900

    (ログイン前の)それっぽいindexページの作成

---
+++ app/Controller/test/test.php
@@ -0,0 +1,42 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Model\Dao\Title;
+
+
+// 会員登録ページコントローラ
+$app->get('/add', function (Request $request, Response $response) {
+
+    //GETされた内容を取得します。
+    $data = $request->getQueryParams();
+
+    // Render index view
+    return $this->view->render($response, 'test/test.twig', $data);
+
+});
+
+// 会員登録処理コントローラ
+$app->post('/add', function (Request $request, Response $response) {
+
+    //POSTされた内容を取得します
+    $data = $request->getParsedBody();
+
+    //ユーザーDAOをインスタンス化
+    $tornament = new Tornament($this->db);
+
+    //入力されたメールアドレスの会員が登録済みかどうかをチェックします
+    if ($tornament->select(array("title" => $data["title"]))) {
+
+        //入力項目がマッチしない場合エラーを出す
+        $data["error"] = "この大会名は入力済みです";
+
+        // 入力フォームを再度表示します
+        return $this->view->render($response, 'test/test.twig', $data);
+
+    }
+
+    // 登録完了ページを表示します。
+    return $this->view->render($response, 'test/test_done.twig', $data);
+
+});
