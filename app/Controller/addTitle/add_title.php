2a3feff9e4b30a9c22e1af9a4de3f1f57e5ef858
kamisho <kamisho.message@icloud.com>
2018-09-11 01:26:06 +0900

    name変更 test→add(新規トーナメント作成ぺージを追加)

--- app/Controller/addTitle/add_title.php
+++ app/Controller/addTitle/add_title.php
@@ -12,7 +12,7 @@ $app->get('/add', function (Request $request, Response $response) {
     $data = $request->getQueryParams();
 
     // Render index view
-    return $this->view->render($response, 'test/test.twig', $data);
+    return $this->view->render($response, 'addTitle/add_title.twig', $data);
 
 });
 
@@ -32,11 +32,11 @@ $app->post('/add', function (Request $request, Response $response) {
         $data["error"] = "この大会名は入力済みです";
 
         // 入力フォームを再度表示します
-        return $this->view->render($response, 'test/test.twig', $data);
+        return $this->view->render($response, 'addTitle/add_title.twig', $data);
 
     }
 
     // 登録完了ページを表示します。
-    return $this->view->render($response, 'test/test_done.twig', $data);
+    return $this->view->render($response, 'addTitle/_done.twig', $data);
 
 });
