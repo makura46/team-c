367d683106738955a6e3a43ba3827a46dd0ad0f5
makura46 <makura6706@gmail.com>
2018-09-11 13:03:13 +0900

    index.phpの修正

--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -8,12 +8,30 @@ use Model\Dao\Theme;
 $app->get('/', function (Request $request, Response $response) {
 
 	$theme = new Theme($this->db); // Themeテーブルのインスタンスを作成
+	$vote = new Vote($this->db); // Voteテーブルのインスタンスを作成
 
 	$param['theme'] = '%';
 
 	$data = $theme->select($param, "", "", 9999, true);  // Themeテーブルから9999件のデータを取得
 
-	for ($data['theme'] as $
+	// テーマIDを利用し、票数の合計を取得
+	for ($i = 0; $i < 9999; $i++) {	
+		$sum = 0;
+		if (isset($data[$i]['themeID']) {
+			$search['themeID'] = $data[$i]['themeID'];
+			$cnt = $theme->select($search, "", "", 9999, true);	// Voteテーブルのカラムを取得
+			for ($j = 0; $j < 9999; $j++) {
+				if (isset($cnt[$j]['vote']) {
+					$sum += $cnt[$j]['vote'];
+				} else {
+					break;
+				}
+			} 
+			$data[$i]['vote'] = $sum;
+		} else {
+			break;
+		}
+	}
 
     // Render index view
     return $this->view->render($response, 'top/index.twig', $data);
