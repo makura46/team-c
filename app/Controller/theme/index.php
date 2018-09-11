0649e34e3cfc973cdd285301e021f2c93b080145
miyabin66 <freezemark5166@gmail.com>
2018-09-11 19:37:09 +0900

    commit

---
+++ app/Controller/theme/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Model\Dao\User;
+use Model\Dao\Theme;
+use Model\Dao\Vote;
+use Model\Dao\Items;
+
+$app->get('/theme/{id}', function (Request $request, Response $response, $args){
+
+    //GETされた内容を取得します。
+    $data = $request->getQueryParams();
+
+    $theme_dao = new Theme($this->db);
+    $items_dao = new Items($this->db);
+
+    $param["id"] = $args['id'];
+    $theme = $theme_dao->select($param, "", "", true);
+    $items = $items_dao->select($param, "", "", true);
+    var_dump($item);
+
+    /*{% for itemId in items %}
+    <p>{{item.name}}</p>
+    {% endfor %}*/
+    
+
+    $data["theme"] = $theme;
+    $data["item"] = $items;
+
+    // Render index view
+    return $this->view->render($response, 'theme/theme.twig', $data);
+
+});
