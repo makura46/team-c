76bc990e8528977a17af7095183d16b19b734f79
miyabin66 <freezemark5166@gmail.com>
2018-09-11 11:26:56 +0900

    revise vote program

--- app/Controller/vote/index.php
+++ app/Controller/vote/index.php
@@ -4,6 +4,7 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 use Model\Dao\User;
 use Model\Dao\Theme;
+use Model\Dao\Vote;
 
 $app->get('/vote/', function (Request $request, Response $response){
 
@@ -18,10 +19,11 @@ $app->post('/vote/', function (Request $request, Response $response) {
     $theme = new Theme($this->db);
 
     $param["title"] = $data["title"];
-    $param["vote"] = $vote["vote"]; 
+    //$param["vote"] = $data["vote"];
+    $param["themeID"] = $data["themeID"];
 
     //入力された情報から会員情報を取得
-    $data = $theme->select($param, "", "", "", true);
+    $data = $theme->select($param, "", "", 9999, true);
 
     // Render index view
     return $this->view->render($response, 'vote/vote.twig', $data);
